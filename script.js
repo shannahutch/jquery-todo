edb1b9b306ef343102e64d6f03fd289268ec5e79
Githubshanna <shannahutch@gmail.com>
2014-06-13 09:39:08 -0700

    clean version

--- script.js
+++ script.js
@@ -2,38 +2,26 @@
 
 $(document).ready(function() {
     
-
     $('#addItem').click(function(e){
-    e.preventDefault();
+        e.preventDefault();
      
-    
-    var userInput = $("#todoItem").val();
-      // $("#todoItem").empty();
-      $("#todoItem").val("");
-    var li = $('<li class="listItem"></li>');
-    li.append('<span class="dotodo">' + userInput + '</span>');
-    var img = $('<img src="http://png-5.findicons.com/files/icons/2226/matte_basic/32/trash_can1.png">');
-    li.append(img);
-    $(".allTodos").append(li);
-      //$( ".allTodos" ).append("<li>" + userInput + "<button class='Remove' type='button'>Remove</button>" "</li>");
-       
+        var userInput = $("#todoItem").val();
+      
+        $("#todoItem").val("");
+        
+        var li = $('<li class="listItem"></li>');
+        li.append('<span class="dotodo">' + userInput + '</span>');
+        var img = $('<img src="http://png-5.findicons.com/files/icons/2226/matte_basic/32/trash_can1.png">');
+        li.append(img);
+        $(".allTodos").append(li);
     });
       
-     // $("li").click(function() {
-     //    $(this).strike();
-
-    
     $(".allTodos").delegate("li", "click", function(){
-     $(this).toggleClass("todo_done");
+        $(this).toggleClass("todo_done");
     });
 
     $(".allTodos").delegate("img", "click", function(e){
-
-     $(e.target).parent().remove();
+        $(e.target).parent().remove();
     });
-    
-       // $(img).remove();
- // $( "p" ).removeClass( "myClass yourClass" )
- // $(".todoItem").fadeOut("slow");
-    // });
+
 });
