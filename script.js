e864030e5ce8f4f0a7acc9be33d76952703958a7
Githubshanna <shannahutch@gmail.com>
2014-06-13 10:35:12 -0700

    final project

--- script.js
+++ script.js
@@ -2,18 +2,52 @@
 
 $(document).ready(function() {
     
+    function deleteAllToDoItems()
+    {
+        // Find todo contains and remove  the contents
+        $('.allTodos').empty();
+    }
+
+    // create click handler for deleteAll
+    $("#deleteAll").click(function(){
+        deleteAllToDoItems();
+    });
+
+    function buildTodoListItem(todoText) {
+        // create empty li
+        var li = $('<li></li>');
+        li.addClass('listItem');
+
+        // create span for input
+        var todoSpan = $('<span></span>');
+        todoSpan.addClass('dotodo');
+        todoSpan.text(todoText);
+        li.append(todoSpan);
+
+        // create trash can image
+        var trashcan = $('<img>');
+        trashcan.attr("src", "http://png-5.findicons.com/files/icons/2226/matte_basic/32/trash_can1.png");
+
+        // append trash can to li
+        li.append(trashcan);
+
+        return li;
+    };
+
+
     $('#addItem').click(function(e){
+
+        // preventing page refresh
         e.preventDefault();
-     
+
+        // get user input from form
         var userInput = $("#todoItem").val();
-      
+
+        // clear user input
         $("#todoItem").val("");
-        
-        var li = $('<li class="listItem"></li>');
-        li.append('<span class="dotodo">' + userInput + '</span>');
-        var img = $('<img src="http://png-5.findicons.com/files/icons/2226/matte_basic/32/trash_can1.png">');
-        li.append(img);
-        $(".allTodos").append(li);
+
+        // append li to allTodos container
+        $(".allTodos").append(buildTodoListItem(userInput));
     });
       
     $(".allTodos").delegate("li", "click", function(){
